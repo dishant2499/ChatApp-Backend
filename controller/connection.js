1d603185b1f41f33592b1adb6d4b28e04980988f
dishant <dishantpanchani122@gmail.com>
2023-08-09 12:23:14 +0530

    change mongodb connection

--- controller/connection.js
+++ controller/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-module.exports.connect = ()=>{mongoose.connect('mongodb://localhost:27017', { useNewUrlParser: true, useUnifiedTopology: true })
+module.exports.connect = ()=>{mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('Connected to database'))
     .catch((err) => console.error('Error connecting to database', err))}

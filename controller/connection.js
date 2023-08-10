515fdfd9185abda92fded003eada02a7e0206788
dishant <dishantpanchani122@gmail.com>
2023-08-10 15:52:21 +0530

    mongodb connection

--- controller/connection.js
+++ controller/connection.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 module.exports.connect = ()=>{mongoose.connect(process.env.MONGODB, { useNewUrlParser: true,
     useUnifiedTopology: true,
-    bufferTimeoutMS: 30000, })
+    })
     .then(() => console.log('Connected to database'))
     .catch((err) => console.error('Error connecting to database', err))}

3801e6f6aea5026d88dbf6e3f2fb75177d8d249e
dishant <dishantpanchani122@gmail.com>
2023-08-10 12:36:24 +0530

    mongodb install

--- controller/connection.js
+++ controller/connection.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
-module.exports.connect = ()=>{mongoose.connect(process.env.MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+module.exports.connect = ()=>{mongoose.connect(process.env.MONGODB, { useNewUrlParser: true,
+    useUnifiedTopology: true,
+    bufferTimeoutMS: 30000, })
     .then(() => console.log('Connected to database'))
     .catch((err) => console.error('Error connecting to database', err))}
